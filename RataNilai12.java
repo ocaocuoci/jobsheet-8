16366215e3b5cc2934f10d1fd41dc645f683e1e1
ocaocuoci <hernandar0606@gmail.com>
2024-10-25 10:25:32 +0700

    mmm

--- RataNilai12.java
+++ RataNilai12.java
@@ -8,7 +8,7 @@ public class RataNilai12 {
 
         i = 1;
         while(i <= 5){ /* outer loop */
-            System.out.print("Input nilai mahasiswa ke-" + i);
+            System.out.println("Input nilai mahasiswa ke-" + i);
             totalNilai = 0;
             for (j =1; j <= 5; j++){ /* inner loop */
                 System.out. print("Nilai ke-" +j + "= ");
@@ -17,7 +17,7 @@ public class RataNilai12 {
             }
             RataNilai = totalNilai / 5; /*menghitung rata nilai yang diinput */
             i++;
-            System.out.print("Nilai rata rata ke-" + i + "adalah " + RataNilai);
+            System.out.println("Nilai rata rata ke-" + i + "adalah " + RataNilai);
 
         } 
         
