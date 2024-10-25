fb244376fed63e2901a477a704182826ed25cc8a
ocaocuoci <hernandar0606@gmail.com>
2024-10-25 09:43:58 +0700

    iOuter = 1 dan for i = 0

--- Square12.java
+++ Square12.java
@@ -7,8 +7,8 @@ public class Square12{
         System.out.print("Masukkan nilai N = ");
         N = sc12.nextInt();
 
-        for(int iOuter = 0; iOuter <= N; iOuter++){
-            for (i = 1; i <= N; i++){
+        for(int iOuter = 1; iOuter <= N; iOuter++){
+            for (i = 0; i <= N; i++){
                 System.out.print("*");
             }
             System.out.println();
